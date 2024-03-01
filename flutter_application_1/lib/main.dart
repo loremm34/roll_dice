7aa75dc76f22317465e2e56d86f5c68944569d7a
loremm34 <161493152+loremm34@users.noreply.github.com>
2024-03-01 21:20:32 +0300

    fixed const widget

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_application_1/gradient_container.dart';
 
 void main() {
   runApp(
-     MaterialApp(
+     const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body:   GradientContainer(
